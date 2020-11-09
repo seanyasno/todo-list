d77035d5e915c3c23238d110e647cca03d2937ab
seanyasno <sean.yasno@gmail.com>
2020-11-09 20:57:33 +0200

    Made an express server.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,23 @@
-console.log('hello, world!');
+import express from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+
+// init
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
+app.use(express.json());
+
+// listen
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+export default app;
